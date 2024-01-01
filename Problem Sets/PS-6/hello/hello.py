b738fdae940e694e95262484227976102ae83472
Sam <108990188+smcnab1@users.noreply.github.com>
2024-01-01 18:54:30 +0000

    Create hello.py

---
+++ Problem Sets/PS-6/hello/hello.py
@@ -0,0 +1,13 @@
+# hello.py
+
+
+def main():
+    # Prompt the user for their name
+    name = input("What is your name? ")
+
+    # Print the personalized greeting
+    print("Hello, " + name + "!")
+
+
+if __name__ == "__main__":
+    main()
